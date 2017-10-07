2fb6ba41a38c9ff160dfb0128d66157862080587
Martín González <m@martingonzalez.net>
2017-10-07 12:38:57 -0400

    Latest data

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 library(dplyr)
+library(plyr)
 library(readr)
 library(tidyr)
 library(googlesheets)
@@ -17,27 +18,33 @@ df14 <- fromJSON("data/ona14.json")
 df15 <- fromJSON("data/ona15.json")
 df16 <- fromJSON("data/ona16.json")
 df17 <- fromJSON("data/ona17.json")
-
 df09 <- fromJSON("data/ona09.json")
 df11 <- fromJSON("data/ona11.json")
 
-df_latest <- bind_rows(df09, df11)
-
-df <- bind_rows(df06, df07, df08, df10, df12, df13,df14, df15, df16, df17)
+df <- bind_rows(df06, df07, df08, df09, df10, df11, df12, df13,df14, df15, df16, df17)
 
 shuffle <- df[sample(nrow(df)),]
 
 write_csv(select(shuffle, -hours), "data/data.csv", na = "")
+write_csv(df_latest, "data/latest.csv", na = "")
 
 # Get data from gdocs
 gs <- gs_url("https://docs.google.com/spreadsheets/d/1A6XDsmHAVk_gC3Uudx8jBZvci66VLXQ-bqSxpj00jnM")
 gs_df <- gs_read(gs)
+gs_chart <- gs_read(gs, ws = "export")
 
+long <- gather(gs_chart, edition, pct, pct_2006:pct_2017)
+long$edition <- substr(long$edition, 5, 9)
+
+# charts ------------------------------------------------------------------
 ggplot(gs_df, aes(edition)) +
   geom_bar(stat = 'count') +
   facet_wrap(~top_cat)
 
-group_by(gs_df, top_cat) %>% 
-  
+slope <- filter(long, edition == 2006 | edition == 2017, categories != 'general')
 
-write_csv(gs_df, "data/categorised.csv")
+ggplot(slope, aes(edition, pct, group = categories, color = categories)) +
+  geom_line() +
+  facet_wrap(~categories)
+
+write_csv(slope, "data/chart.csv")
